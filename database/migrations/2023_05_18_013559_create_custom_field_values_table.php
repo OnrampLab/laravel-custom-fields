91fa9d258a1651093712c400324949084b308676
Luna <luna.moon.brillar@gmail.com>
2023-05-18 17:44:48 +0800

    chore(migration): [custom_field_values] create table

---
+++ database/migrations/2023_05_18_013559_create_custom_field_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_field_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('custom_field_id')->constrained();
+            $table->string('value');
+            $table->morphs('customizable');
+            $table->index(['custom_field_id', 'value']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_field_values');
+    }
+};
