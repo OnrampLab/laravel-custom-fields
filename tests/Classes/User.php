f2e767a16048516a8f43c32f7a9dbfd3453d6cba
Luna <luna.moon.brillar@gmail.com>
2023-05-31 14:13:26 +0800

    chore(migration): [users] add account_id to users table

---
+++ tests/Classes/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OnrampLab\CustomFields\Tests\Classes;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as BaseUser;
+use OnrampLab\CustomFields\Concerns\Customizable;
+
+class User extends BaseUser
+{
+    use HasFactory;
+    use Customizable;
+    protected $guarded = [];
+
+    protected static function newFactory(): Factory
+    {
+        return UserFactory::new();
+    }
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function getContext(): Model
+    {
+        return $this->account;
+    }
+}
